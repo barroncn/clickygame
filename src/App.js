02459697ba33d6c6f6b307da587984436faa12ed
Caitlin Barron <barroncn@gmail.com>
2018-01-12 17:44:13 +0000

    working on public folder

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Image from "./components/Image";
 import NavBar from "./components/NavBar";
 import Jumbotron from "./components/Jumbotron";
 import Footer from "./components/Footer";
-import Modal from ".//components/Modal";
 import './App.css';
 
 class App extends Component {
@@ -15,49 +14,57 @@ class App extends Component {
     message: "Click a picture to start!"
   };
 
+  //The shuffle function that is called below...
+  shuffle = () => {
+    //randomly sorts the cards
+    this.state.cards.sort(function(a, b) { return 0.5 - Math.random() });
+    //Displays the newly sorted cards
+    this.setState({ cards });
+  }
+
   clicked = (id) => {
-
-
     //our array of cards
     const images = this.state.cards;
 
     //the card we clicked on (this will be an array with one card/image in it)
     const clickedCard = images.filter(image => image.id === id);
-    console.log(clickedCard);
+
     //if the user has already clicked on the card...
     if (clickedCard[0].isClicked) {
-      // this.state.userCounter = 0;
-      this.setState({ userCounter: 0 });
-      images.forEach(image => image.isClicked = false)
-      this.setState({ cards: images })
-      this.shuffle();
-      this.setState({ message: "Oops, guessed it twice! Start again..." });
+      //Change each image's isClicked key to the value false
+      images.forEach(image => image.isClicked = false);
+      //Set the states: all cards are set to unclicked in DOM, user counter equal to 0 is displayed, and oops message is displayed
+      this.setState({
+        cards: images,
+        userCounter: 0,
+        message: "Oops, guessed it twice! Start again..."
+      });
     }
 
     //the user clicked a new card...
     else {
-      this.setState({ message: "Nice Guess! Keep it Up!" })
-
-      //increment the user's click counter (not updated on screen yet)
-      this.setState({ userCounter: this.state.userCounter + 1 });
-
+      //Change the isClicked value of the clicked card to true
       clickedCard[0].isClicked = true;
-      this.setState({ cards: images });
+      //Set the states: cards clicked values updated in DOM, user counter incremented and displayed, and nice guess message is displayed
+      this.setState({
+        cards: images,
+        userCounter: this.state.userCounter + 1,
+        message: "Nice Guess! Keep it Up!"
+      });
 
+      //If the user has the highest score...
       if (this.state.userCounter >= this.state.highScore) {
+        //Increment the high score value and display it
         this.setState({ highScore: this.state.highScore + 1 });
+        //Shuffle the cards
         this.shuffle();
       }
+      //If the user does not have the highest score...
       else {
+        //Shuffle the cards
         this.shuffle();
       }
     }
-  }
-
-  shuffle = () => {
-    this.state.cards.sort(function(a, b) { return 0.5 - Math.random() });
-
-    this.setState({ cards });
   }
 
   render() {
@@ -80,8 +87,7 @@ class App extends Component {
           key= {image.id}
           id={image.id}
           clicked= {this.clicked}
-          shuffle= {this.shuffle}
-          shake={this.shake}
+          // shake={this.shake}
           />
         ))}
       </div>,
--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Image.css";
 
 const Image = (props) => (
-    <img src={props.url} alt={props.name} data-click={props.clickedYet} className="images rounded" onClick={() => props.clicked(props.id)}></img>
+    <img src={props.url} alt={props.name} className="images rounded" onClick={() => props.clicked(props.id)}></img>
 );
 
 export default Image;
